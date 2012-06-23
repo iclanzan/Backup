222aeb925344dba89fbff4d1a7431597beb591d5
Sorin Iclanzan <sorin@iclanzan.com>
2012-06-23 14:49:23 +0300

    Changed the version back to 2.0 because we're in the stable master branch.

--- backup.php
+++ backup.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Backup
-Version: 2.1
+Version: 2.0
 Plugin URI: http://hel.io/wordpress/backup/
 Description: Backup your WordPress website to Google Drive.
 Author: Sorin Iclanzan
