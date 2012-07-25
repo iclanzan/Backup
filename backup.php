3a172280c1e43126a0aa144feeaddb9aa22ffaac
Sorin Iclanzan <iclanzan@macbook>
2012-07-25 14:13:54 +0300

    Bumped the version to accommodate for the last commit.

--- backup.php
+++ backup.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Backup
-Version: 2.1
+Version: 2.1.1
 Plugin URI: http://hel.io/wordpress/backup/
 Description: Back up your WordPress website to Google Drive.
 Author: Sorin Iclanzan
